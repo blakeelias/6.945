0d2f9307c3ffdf0ab2732bc35651f96c15d925e8
Blake Elias <blakeelias@gmail.com>
2017-02-12 19:07:47 -0500

    Fix import, and add answers for 1 (a)

--- ps00/p0.scm
+++ ps00/p0.scm
@@ -1,8 +1,34 @@
-(load "p0utils.scm")
+(load "~/Dropbox (MIT)/Classes/6.945/ps00/p0utils.scm")
 
+#|
+Problem 1
+|#
 
+; Question (a)
 
+(modulo 13 8)
+;Value: 5
 
+(remainder 13 8)
+;Value: 5
+
+(modulo -13 8)
+;Value: 3
+
+(remainder -13 8)
+;Value: -5
+
+(modulo -13 -8)
+;Value: -5
+
+(remainder -13 -8)
+;Value: -5
+
+; Question (b)
+
+#|
+
+|#
 
 
 
