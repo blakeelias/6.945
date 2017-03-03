41382723b5d86b1dc6102dc74c073af9996c1611
Blake Elias <blakeelias@gmail.com>
2017-03-03 01:01:58 -0500

    Install boolean arithmetic (and reset environment at the beginning of each load)

--- ps02/ps02.scm
+++ ps02/ps02.scm
@@ -1,13 +1,15 @@
+(ge (make-top-level-environment))
 (cd "/Users/blake/Dropbox\ (MIT)/Classes/6.945/ps02/code")
 (load "load.scm")
 
-;;; Problem 2.1 Warmup
+;;; Problem 2.1 Warmup 
 
 (define boolean-arithmetic
   (make-arithmetic 'boolean boolean? '()
 		   (lambda (name)
 		     (case name
 		       ((additive-identity) #f)
+		       ((negate) #t)
 		       ((multiplicative-identity) #t)
 		       (else (default-object))))
   (lambda (operator)
@@ -20,4 +22,6 @@
       (and procedure
 	   (simple-operation operator boolean? procedure))))))
 
+(install-arithmetic! boolean-arithmetic)
 
+; (install-arithmetic! symbolic-arithmetic-1)
