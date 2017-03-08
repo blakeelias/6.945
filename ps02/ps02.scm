bf1d40b62222c62d995ae00baf3d1c38909d2b6c
Blake Elias <blakeelias@gmail.com>
2017-03-07 20:53:25 -0500

    Define vector addition function; draft of a vector-arithmetic which does not yet work

--- ps02/ps02.scm
+++ ps02/ps02.scm
@@ -48,5 +48,58 @@
 
 
 ;;; Problem 2.2
+; (a)
+
+(ge (make-top-level-environment))
+(cd "/Users/blake/Dropbox\ (MIT)/Classes/6.945/ps02/code")
+(load "load.scm")
+
+(register-predicate! vector? 'vector)
+
+(define (ensure-vector-lengths-match vecs)
+  ( let (( first-vec-length ( vector-length ( car vecs ))))
+    ( if ( any ( lambda ( v)
+		 ( not (n:= ( vector-length v)
+			  first-vec-length )))
+	       vecs)
+	 (error "Vector dimension mismatch:" vecs))))
+
+(define (vector-element-wise element-procedure)
+  (lambda vecs
+    (ensure-vector-lengths-match vecs)
+    (apply vector-map element-procedure vecs)))
+
+((vector-element-wise (lambda (x) (n:* x 2))) #(1 2 3))
+;Value 153: #(2 4 6)
+
+(define (sum l) (reduce + 0 l))
+
+(define (v:+ vector1 vector2)
+  (ensure-vector-lengths-match (list vector1 vector2))
+  (list->vector
+   (map sum (zip (vector->list vector1) (vector->list vector2)))))
 
 
+#|
+(define vector-arithmetic
+  (make-arithmetic 'vector vector? '()
+		   (lambda (name)
+		     (case name
+		       ((additive-identity) 0)
+		       ((multiplicative-identity) 1)
+		       (else (default-object))))
+  (lambda (operator)
+    (let ((procedure
+	   (case operator
+	     ((+) (lambda (x y) (vec-add x y)))
+	     ((-) (lambda (x y) (vec-sub x y)))
+	     ((*) (lambda (x y) (vec-dot x y)))
+	     ((negate) (lambda (x) (vec-mult x -1)))
+	     (else
+	      (lambda args
+		(error "Operator undefined in Boolean" operator))))))
+      (and procedure
+	   (simple-operation operator boolean? procedure))))))
+
+(install-arithmetic! vector-arithmetic)
+|#
