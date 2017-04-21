e7a328d3d255d6faeb89063a3eba0a42269c9073
Blake Elias <blakeelias@gmail.com>
2017-04-20 21:11:13 -0400

    Warm-up for ps04

--- ps04/ps04.scm
+++ ps04/ps04.scm
@@ -115,7 +115,7 @@
       n
       (+ (fib (- n 1)) (fib (- n 2)))))
 
-; (with-predicate-counts ( lambda () ( fib 20) ))
+(with-predicate-counts ( lambda () ( fib 20) ))
 
 #|
 ;Results:
@@ -134,7 +134,79 @@
 ;Value: 6765
 |#
 
-(with-predicate-counts (lambda () (test-stormer-counts)))
+(ge (make-top-level-environment))
+(cd "/Users/blake/Dropbox\ (MIT)/Classes/6.945/ps04/code")
+(load "load.scm")
 
-;(cd "/Users/blake/Dropbox\ (MIT)/Classes/6.945/ps04/code")
-;(load "load.scm")
+#|
+(start-adventure 'Blake)
+You are in student-street
+You see here: sicp
+You can exit: skew up in down west
+;Unspecified return value
+
+
+(take-thing 'sicp)
+blake picks up sicp
+;Unspecified return value
+
+(go 'up)
+blake leaves via the up exit
+blake enters gates-tower
+You are in gates-tower
+Your bag contains: sicp
+You can see: dreyfoos-tower
+You can exit: down
+;Unspecified return value
+
+(go 'down)
+blake leaves via the down exit
+blake enters student-street
+blake says: Hi ben-bitdiddle alyssa-hacker course-6-frosh
+You are in student-street
+Your bag contains: sicp
+You see here: ben-bitdiddle alyssa-hacker course-6-frosh
+You can exit: skew up in down west
+course-6-frosh leaves via the in exit
+alyssa-hacker leaves via the west exit
+ben-bitdiddle leaves via the skew exit
+;Unspecified return value
+
+(go 'in)
+blake leaves via the in exit
+blake enters 32-123
+blake says: Hi course-6-frosh
+You are in 32-123
+Your bag contains: sicp
+You see here: problem-set course-6-frosh
+You can exit: out
+course-6-frosh leaves via the out exit
+;Unspecified return value
+
+(take-thing 'problem-set)
+blake picks up problem-set
+;Unspecified return value
+
+(go 'up)
+No exit in up direction
+;Unspecified return value
+
+(go 'west)
+No exit in west direction
+;Unspecified return value
+
+(go 'out)
+blake leaves via the out exit
+blake enters student-street
+blake says: Hi ben-bitdiddle course-6-frosh
+You are in student-street
+Your bag contains: problem-set sicp
+You see here: ben-bitdiddle course-6-frosh
+You can exit: skew up in down west
+course-6-frosh leaves via the down exit
+ben-bitdiddle takes sicp from blake
+blake says: Yaaaah! I am upset!
+An earth-shattering, soul-piercing scream is heard...
+;Unspecified return value
+|#
+
